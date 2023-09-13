cf62ac2ff832f23375a76e78455d5806adacad2b
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 19:25:04 +0200

    add the vertxes for the road

--- src/main.c
+++ src/main.c
@@ -7,8 +7,22 @@ int main(int argc, char* argv[])
     sfRenderWindow *window = sfRenderWindow_create(mode, "my_car", sfDefaultStyle, NULL);
     sfEvent event;
     sfVertexArray *road_right = sfVertexArray_create();
-    sfVertex v1 = {
+    VERTEX_DECLA(v1, 1282, 742, sfWhite);
     sfVertexArray_append(road_left, v1);
+    VERTEX_DECLA(v2, 1293, 1206, sfWhite);
+    sfVertexArray_append(road_left, v2);
+    VERTEX_DECLA(v3, 1580, 1436, sfWhite);
+    sfVertexArray_append(road_left, v3);
+    VERTEX_DECLA(v4, 2133, 1460, sfWhite);
+    sfVertexArray_append(road_left, v4);
+    VERTEX_DECLA(v5, 2424, 1168, sfWhite);
+    sfVertexArray_append(road_left, v5);
+    VERTEX_DECLA(v6, 2430, 766, sfWhite);
+    sfVertexArray_append(road_left, v6);
+    VERTEX_DECLA(v7, 2136, 497, sfWhite);
+    sfVertexArray_append(road_left, v7);
+    VERTEX_DECLA(v8, 1582, 493, sfWhite);
+    sfVertexArray_append(road_left, v8);
     while (sfRenderWindow_isOpen(window)) {
         while (sfRenderWindow_pollEvent(window, &event)) {
             if (event.type == sfEvtClosed)
