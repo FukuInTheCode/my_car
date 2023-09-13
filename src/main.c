722f55f110d6e727f480c5894cec193c08756994
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 20:07:34 +0200

    bug fix

--- src/main.c
+++ src/main.c
@@ -9,7 +9,8 @@ int main(int argc, char* argv[])
     sfVertexArray *road_r = sfVertexArray_create();
     sfVertexArray *road_l = sfVertexArray_create();
     sfVertexArray *current_border = road_r;
-    sfVertexArray_setPrimitiveType(current_border, sfLines);
+    sfVertexArray_setPrimitiveType(road_l, sfLines);
+    sfVertexArray_setPrimitiveType(road_r, sfLines);
     sfVector2i window_pos = sfRenderWindow_getPosition(window);
     bool ended = false;
     bool clicked = false;
