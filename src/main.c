5835c9b6df2e2b3567aaf158435b105a44cd4b9f
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 19:35:44 +0200

    change

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(int argc, char* argv[])
         }
         if (sfKeyboard_isKeyPressed(sfKeyA)) {
             sfVertex tmp = {
-                sfMouse_getPosition(window),
+                sfMouse_getPosition(NULL),
                 sfWhite,
                 {0, 0}
             };
