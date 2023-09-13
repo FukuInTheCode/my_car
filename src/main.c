044830e68f7d73edb4b1bbe39cc02adea81acb23
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 19:56:02 +0200

    chnaeh in main

--- src/main.c
+++ src/main.c
@@ -6,16 +6,6 @@ int main(int argc, char* argv[])
     sfVideoMode mode = {2000, 1500, 32};
     sfRenderWindow *window = sfRenderWindow_create(mode, "my_car", sfDefaultStyle, NULL);
     sfEvent event;
-    // sfVertex road_right[] = {
-    //     {{1282, 742}, sfColor_fromRGB(255, 255, 255)},
-    //     {{1293, 1206}, sfColor_fromRGB(255, 255, 255)},
-    //     {{1580, 1436}, sfColor_fromRGB(255, 255, 255)},
-    //     {{2133, 1460}, sfColor_fromRGB(255, 255, 255)},
-    //     {{2424, 1168}, sfColor_fromRGB(255, 255, 255)},
-    //     {{2430, 766}, sfColor_fromRGB(255, 255, 255)},
-    //     {{2136, 497}, sfColor_fromRGB(255, 255, 255)},
-    //     {{1582, 493}, sfColor_fromRGB(255, 255, 255)}
-    // };
     sfVertexArray *road_r = sfVertexArray_create();
     sfVertexArray *road_l = sfVertexArray_create();
     sfVertexArray *current_border = road_r;
@@ -43,8 +33,11 @@ int main(int argc, char* argv[])
             clicked = false;
         if (sfKeyboard_isKeyPressed(sfKeyZ) && !ended) {
             sfVertexArray_append(current_border, *sfVertexArray_getVertex(current_border, 0));
-            ended = true;
             clicked = false;
+            if (current_border == road_l)
+                ended = true;
+            else
+                current_border = road_l;
         }
         sfRenderWindow_clear(window, sfColor_fromRGBA(44, 44, 44, 128));
         sfRenderWindow_drawVertexArray(window, current_border, NULL);
