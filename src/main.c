ad48fe5aa5ea47339ee21e1ba1c1508d7d2df384
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 19:21:19 +0200

    add a macrofor the vertexes

--- src/main.c
+++ src/main.c
@@ -6,13 +6,15 @@ int main(int argc, char* argv[])
     sfVideoMode mode = {2000, 1500, 32};
     sfRenderWindow *window = sfRenderWindow_create(mode, "my_car", sfDefaultStyle, NULL);
     sfEvent event;
+    sfVertexArray *road_right = sfVertexArray_create();
+    sfVertex v1 = {
+    sfVertexArray_append(road_left, v1);
     while (sfRenderWindow_isOpen(window)) {
         while (sfRenderWindow_pollEvent(window, &event)) {
             if (event.type == sfEvtClosed)
                 sfRenderWindow_close(window);
         }
-        if (sfKeyboard_isKeyPressed(sfKeyA))
-            printf("%d, %d\n", sfMouse_getPosition(NULL).x, sfMouse_getPosition(NULL).y);
+
         sfRenderWindow_clear(window, sfColor_fromRGBA(44, 44, 44, 128));
         sfRenderWindow_display(window);
     }
