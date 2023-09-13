ae811e4cb558bfacdaa75071f033bea44cf829a8
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 19:08:10 +0200

    creatae a window loop

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include "../includes/my.h"
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    sfVideoMode mode = {2000, 1500, 32};
+    sfRenderWindow *window = sfRenderWindow_create(mode, "my_car", sfDefaultStyle, NULL);
+    sfEvent event;
+    while (sfRenderWindow_isOpen(window)) {
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            if (event->type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+        sfRenderWindow_clear(window, sfColor_fromRGBA(44, 44, 44, 128));
+        sfRenderWindow_display(window);
+    }
+    sfRenderWindow_destroy(window);
     return 0;
 }
