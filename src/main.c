0df473a292b7fa19ec99d1cddb1bf4d5a8be6f32
FukuInTheCode <tomc.code@gmail.com>
2023-09-14 13:52:57 +0200

    chaneg

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
     //     {100 * cos(), 100},
     //     {}
     // };
-    double sight_angles[] = {0, PI / 2, - PI / 2};
+    double sight_angles[] = {0, PI / 2, - PI / 2, PI / 3, - PI / 3};
     uint32_t sight_l_n = sizeof(sight_angles) / sizeof(double);
     double sight_powah = 100.;
     // main
