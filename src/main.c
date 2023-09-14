2c6dda778ece5318bfd0b4372220d1d866a68ad0
FukuInTheCode <tomc.code@gmail.com>
2023-09-14 22:46:46 +0200

    changes

--- src/main.c
+++ src/main.c
@@ -194,10 +194,8 @@ int main(int argc, char* argv[])
             if (is_intersec) {
                 sfRectangleShape_setFillColor(car_rect, sfGreen);
                 is_crash = true;
-            } else {
-                is_crash = false;
+            } else
                 sfRectangleShape_setFillColor(car_rect, sfRed);
-            }
 
             MAT_DECLA(inputs);
             my_matrix_create(sight_l_n * 2 + 2, 1, 1, &inputs);
@@ -251,7 +249,6 @@ int main(int argc, char* argv[])
             MAT_DECLA(pred);
             my_nn_predict(&car_brain, &inputs, &pred);
             uint32_t id = my_matrix_find_row_index(&pred, 0, my_matrix_max(&pred));
-            printf("%i\n", is_crash);
             if (id == 0 && car_angular_velocity > - max_angular_speed && !is_crash)
                 car_angular_acceleration = -1;
             if (id == 1 && car_angular_velocity < max_angular_speed && !is_crash)
