e327053a5f822bd661f4279b521ebfb7a336ca89
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 11:17:17 +0200

    check alloc

--- src/func/my_map_init.c
+++ src/func/my_map_init.c
@@ -1,7 +1,9 @@
+#define MATRIX_CHECK_ALLOC
 #include "../../includes/my.h"
 
 void my_map_init(my_map_t *map)
 {
     map->walls = calloc(1, sizeof(sfVertexArray));
+    check_alloc(map->walls);
     map->walls_n = 1;
 }
--- src/func/my_map_wall.c
+++ src/func/my_map_wall.c
@@ -2,4 +2,8 @@
 
 void my_map_draw_wall(my_map_t *map, sfRenderWindow *window)
 {
+    if (sfKeyboard_isKeyPressed(sfKeyW))
+        return; // TODO
+
+    
 }
