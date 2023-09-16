5e70a5bb6b9866acad26ecbf1c2bd723b943bb44
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 11:31:45 +0200

    bug fix

--- src/func/my_map_wall.c
+++ src/func/my_map_wall.c
@@ -14,7 +14,7 @@ void my_map_draw_wall(my_map_t *map, sfRenderWindow *window)
 {
     if (sfKeyboard_isKeyPressed(sfKeyW)) {
         map->status = no_mode;
-        sfVertex pt = sfVertexArray_getVertex(map->walls[map->walls_n - 1], 0);
+        sfVertex pt = *sfVertexArray_getVertex(map->walls[map->walls_n - 1], 0);
         sfVertexArray_append(map->walls[map->walls_n], pt);
         my_map_add_wall(map);
     }
