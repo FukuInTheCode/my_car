90a7128ae82e12daa5388acd49f1174f51db644d
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 14:38:02 +0200

    add a alloc gates func

--- src/func/my_map_gate.c
+++ src/func/my_map_gate.c
@@ -2,12 +2,12 @@
 
 void my_map_alloc_gate(my_map_t *map)
 {
-    sfVertexArray **tmp_walls = map->walls;
-    map->walls = calloc(map->walls_n + 1, sizeof(sfVertexArray *));
-    check_alloc(map->walls);
-    for (uint32_t i = 0; i < map->walls_n; ++i)
-        map->walls[i] = tmp_walls[i];
-    map->walls_n += 1;
-    map->walls[map->walls_n - 1] = sfVertexArray_create();
-    sfVertexArray_setPrimitiveType(map->walls[map->walls_n - 1], sfLines);
+    sfVertexArray **tmp_gates = map->gates;
+    map->walls = calloc(map->gates_n + 1, sizeof(sfVertexArray *));
+    check_alloc(map->gates);
+    for (uint32_t i = 0; i < map->gates_n; ++i)
+        map->gates[i] = tmp_gates[i];
+    map->gates_n += 1;
+    map->gates[map->gates_n - 1] = sfVertexArray_create();
+    sfVertexArray_setPrimitiveType(map->gates[map->gates_n - 1], sfLines);
 }
