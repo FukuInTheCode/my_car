95404d766fc898b7c2517b1ad016fcd356f6619c
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 10:56:36 +0200

    add so boolean

--- src/func/my_map_create.c
+++ src/func/my_map_create.c
@@ -12,9 +12,11 @@ static void handle_event(sfRenderWindow *window)
 void my_map_create(my_map_t *map, sfVideoMode mode)
 {
     sfRenderWindow *window = sfRenderWindow_create(mode, "create race", sfDefaultStyle, NULL);
+    bool wall_mode = false;
+    bool gate_mode = false;
+    bool start_mode = false;
     while (sfRenderWindow_isOpen(window)) {
         handle_event(window);
-
     }
     sfRenderWindow_destroy(window);
 }
