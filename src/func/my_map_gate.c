dc5597fd965900828e662f34643906ba3ce23322
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 14:52:57 +0200

    bug fix

--- src/func/my_map_gate.c
+++ src/func/my_map_gate.c
@@ -4,7 +4,7 @@
 void my_map_alloc_gate(my_map_t *map)
 {
     sfVertexArray **tmp_gates = map->gates;
-    map->walls = calloc(map->gates_n + 1, sizeof(sfVertexArray *));
+    map->gates = calloc(map->gates_n + 1, sizeof(sfVertexArray *));
     check_alloc(map->gates);
     for (uint32_t i = 0; i < map->gates_n; ++i)
         map->gates[i] = tmp_gates[i];
