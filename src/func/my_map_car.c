16fef999d9b3f804d53088df361b9595bd516f3e
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 15:09:30 +0200

    change

--- src/func/my_map_car.c
+++ src/func/my_map_car.c
@@ -7,5 +7,6 @@ void my_map_place_start(my_map_t *map, sfRenderWindow *window)
         sfVector2u mouse_pos = sfMouse_getPosition(NULL);
         map->start.x = mouse_pos.x - window_pos.y;
         map->start.y = mouse_pos.y - window_pos.y;
+        map->status = race_mode;
     }
 }
