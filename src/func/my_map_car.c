d117efac29afa40134c185c3385f296c285ecebd
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 15:13:04 +0200

    bug fix

--- src/func/my_map_car.c
+++ src/func/my_map_car.c
@@ -3,8 +3,8 @@
 void my_map_place_start(my_map_t *map, sfRenderWindow *window)
 {
     if (sfKeyboard_isKeyPressed(sfKeySpace)) {
-        sfVector2u window_pos = sfRenderWindow_getPosition(window);
-        sfVector2u mouse_pos = sfMouse_getPosition(NULL);
+        sfVector2i window_pos = sfRenderWindow_getPosition(window);
+        sfVector2i mouse_pos = sfMouse_getPosition(NULL);
         map->start.x = mouse_pos.x - window_pos.y;
         map->start.y = mouse_pos.y - window_pos.y;
         map->status = no_mode;
