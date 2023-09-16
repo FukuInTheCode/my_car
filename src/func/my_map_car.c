4f3b57f2243d634b286cac65e278429b9428f65c
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 15:08:55 +0200

    chanegs

---
+++ src/func/my_map_car.c
@@ -0,0 +1,11 @@
+#include "../../includes/my.h"
+
+void my_map_place_start(my_map_t *map, sfRenderWindow *window)
+{
+    if (sfKeyboard_isKeyPressed(sfKeySpace)) {
+        sfVector2u window_pos = sfRenderWindow_getPosition(window);
+        sfVector2u mouse_pos = sfMouse_getPosition(NULL);
+        map->start.x = mouse_pos.x - window_pos.y;
+        map->start.y = mouse_pos.y - window_pos.y;
+    }
+}
