e38af6d72bed542ab18396a1d71baf2377d90b8a
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 10:50:16 +0200

    add a static handle event

--- src/func/my_map_create.c
+++ src/func/my_map_create.c
@@ -1,6 +1,19 @@
 #include "../../includes/my.h"
 
+static void handle_event(sfRenderWindow *window)
+{
+    sfEvent event;
+    while (sfRenderWindow_pollEvent(window, &event)) {
+        if (event.type == sfEvtClosed)
+            sfRenderWindow_close(window);
+    }
+}
+
 void my_map_create(my_map_t *map, sfVideoMode mode)
 {
     sfRenderWindow *window = sfRenderWindow_create(mode, "create race", sfDefaultStyle, NULL);
+    while (sfRenderWindow_isOpen(window)) {
+
+    }
+    sfRenderWindow_destroy(window);
 }
