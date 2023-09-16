37450ce2e40f3413c67a9db651cbd38929622b13
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 11:04:56 +0200

    del the bool

--- src/func/my_map_create.c
+++ src/func/my_map_create.c
@@ -14,9 +14,6 @@ static void handle
 void my_map_create(my_map_t *map, sfVideoMode mode)
 {
     sfRenderWindow *window = sfRenderWindow_create(mode, "create race", sfDefaultStyle, NULL);
-    bool wall_mode = false;
-    bool gate_mode = false;
-    bool start_mode = false;
     while (sfRenderWindow_isOpen(window)) {
         handle_event(window);
         handle_keyboard(map, window);
