08d8dbe684598dcfeb227413ec5e67442a1ed982
FukuInTheCode <tomc.code@gmail.com>
2023-09-16 11:21:31 +0200

    can draw wall

--- src/func/my_map_wall.c
+++ src/func/my_map_wall.c
@@ -5,5 +5,12 @@ void my_map_draw_wall(my_map_t *map, sfRenderWindow *window)
     if (sfKeyboard_isKeyPressed(sfKeyW))
         return; // TODO
 
-    
+    if (sfKeyboard_isKeyPressed(sfKeySpace)) {
+        sfVector2i window_pos = sfRenderWindow_getPosition(window);
+        sfVector2i pos = sfMouse_getPosition(NULL);
+        pos.x -= window_pos.x;
+        pos.y -= window_pos.y;
+        sfVertex pt = {pos, sfWhite, {0, 0}};
+        sfVertexArray_append(map->walls[map->walls_n - 1], pt);
+    }
 }
