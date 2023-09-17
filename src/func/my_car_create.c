820cbefc431aeb828f3698c0c1be8f69ba40fd8e
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 22:58:10 +0200

    start of the my_car _create func

--- src/func/my_car_create.c
+++ src/func/my_car_create.c
@@ -1,3 +1,13 @@
 #include "../../includes/my.h"
 
-
+void my_car_create(my_car_t *car)
+{
+    max_rota_speed = 0;
+    max_speed = 0;
+    velocity = 0;
+    angle_velocity = 0;
+    acceleration = 0;
+    angle_acceleration = 0;
+    current_gate = 0;
+    reward = 0;
+}
