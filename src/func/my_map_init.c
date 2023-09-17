ba2ba3c5a7c601cee9fdc226351d243685337be0
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 20:49:33 +0200

    bug fix

--- src/func/my_map_init.c
+++ src/func/my_map_init.c
@@ -4,6 +4,7 @@ void my_map_init(my_map_t *map)
 {
     map->status = no_mode;
     map->walls_n = 0;
+    map->walls = NULL;
     my_map_alloc_wall(map);
     map->gates_n = 0;
     my_map_alloc_gate(map);
