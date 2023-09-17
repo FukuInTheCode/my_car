ff570b551c95c0465f664c74ce7690156afbb1b0
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 23:00:54 +0200

    chaneg

--- src/func/my_car_create.c
+++ src/func/my_car_create.c
@@ -1,6 +1,6 @@
 #include "../../includes/my.h"
 
-void my_car_create(my_car_t *car)
+void my_car_create(my_car_t *car, my_map_t *map)
 {
     max_rota_speed = 0;
     max_speed = 0;
@@ -8,6 +8,6 @@ void my_car_create(my_car_t *car)
     angle_velocity = 0;
     acceleration = 0;
     angle_acceleration = 0;
-    current_gate = 0;
+    current_gate = find_current_gate(car, map);
     reward = 0;
 }
