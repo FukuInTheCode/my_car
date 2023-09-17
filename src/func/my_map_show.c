119a9c9c68309ea1df6c599271b729a8deaab2c4
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 22:36:15 +0200

    move the handle show to it own file

--- src/func/my_map_show.c
+++ src/func/my_map_show.c
@@ -1,3 +1,11 @@
 #include "../../includes/my.h"
 
-
+void my_map_draw(my_map_t *map, sfRenderWindow *window)
+{
+    sfRenderWindow_clear(window, sfBlack);
+    for (uint32_t i = 0; i < map->walls_n; ++i)
+        sfRenderWindow_drawVertexArray(window, map->walls[i], NULL);
+    for (uint32_t i = 0; i < map->gates_n; ++i)
+        sfRenderWindow_drawVertexArray(window, map->gates[i], NULL);
+    sfRenderWindow_display(window);
+}
