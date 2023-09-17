c88d0be4420e5e597ecbdf53f6ed2b6d19b2e7be
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 23:09:22 +0200

    change

--- src/func/my_car_create.c
+++ src/func/my_car_create.c
@@ -1,5 +1,16 @@
 #include "../../includes/my.h"
 
+static void find_current_gate(my_car_t *car, my_map_t *map)
+{
+    sfFloatRect gate_rect = sfVertexArray_getBounds(map->gates[0]);
+    double closer_dist = pow(gate_rect.top + gate_rect.height / 2., 2);
+    closer_dist += pow(gate_rect.left + gate_rect.width / 2., 2);
+    closer_dist = sqrt(closer_dist);
+    for (uint32_t i = 1; i < map->gates_n; ++i) {
+        double dist = 
+    }
+}
+
 void my_car_create(my_car_t *car, my_map_t *map)
 {
     max_rota_speed = 0;
