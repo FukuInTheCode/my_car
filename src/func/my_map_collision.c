ac093ad27b58a7138c9b300b8e8ebf4b60cba107
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 21:15:57 +0200

    add a map collided gate func

--- src/func/my_map_collision.c
+++ src/func/my_map_collision.c
@@ -5,12 +5,29 @@ bool my_map_is_intersecting_wall(my_map_t *map, sfVertex *line,\
                                                     sfVector2f *res)
 {
     bool is_intersect = false;
-    for (uint32_t i = 0; i < map->walls_n && !is_intersect; ++i) {
+    for (uint32_t i = 0; i < map->gates_n && !is_intersect; ++i) {
         size_t count = sfVertexArray_getVertexCount(map->walls[i]);
         for (uint32_t j = 0; j < count && !is_intersect; j += 2){
             sfVertex wall[] = {
                 *sfVertexArray_getVertex(map->walls[i], j),
                 *sfVertexArray_getVertex(map->walls[i], j + 1)
+            };
+            is_intersect = is_intersecting(line, wall, res);
+        }
+    }
+    return is_intersect;
+}
+
+bool my_map_is_intersecting_gate(my_map_t *map, sfVertex *line,\
+                                                    sfVector2f *res)
+{
+    bool is_intersect = false;
+    for (uint32_t i = 0; i < map->gates_n && !is_intersect; ++i) {
+        size_t count = sfVertexArray_getVertexCount(map->gates[i]);
+        for (uint32_t j = 0; j < count && !is_intersect; j += 2){
+            sfVertex wall[] = {
+                *sfVertexArray_getVertex(map->gates[i], j),
+                *sfVertexArray_getVertex(map->gates[i], j + 1)
             };
             is_intersect = is_intersecting(line, wall, res);
         }
