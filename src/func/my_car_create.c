ada37ef90254493b4c0e6193900f699efc87eec8
FukuInTheCode <tomc.code@gmail.com>
2023-09-17 23:14:00 +0200

    bug fix

--- src/func/my_car_create.c
+++ src/func/my_car_create.c
@@ -21,12 +21,13 @@ static void find_current_gate(my_car_t *car, my_map_t *map)
 
 void my_car_create(my_car_t *car, my_map_t *map)
 {
-    max_rota_speed = 0;
-    max_speed = 0;
-    velocity = 0;
-    angle_velocity = 0;
-    acceleration = 0;
-    angle_acceleration = 0;
-    reward = 0;
+    car->max_rota_speed = 0;
+    car->max_speed = 0;
+    car->velocity = 0;
+    car->angle_velocity = 0;
+    car->acceleration = 0;
+    car->angle_acceleration = 0;
+    car->reward = 0;
+    
     find_current_gate(car, map);
 }
