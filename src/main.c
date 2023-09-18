0c16eaaf801f6a004fa8fb2bd28992e37ab1fc20
FukuInTheCode <tomc.code@gmail.com>
2023-09-18 22:58:13 +0200

    chnaeg in h

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ int main(int argc, char* argv[])
                 sfRenderWindow_close(window);
         }
         my_map_draw(&map, window);
+        my_car_draw(window, &car, &map);
     }
     sfRenderWindow_destroy(window);
 
