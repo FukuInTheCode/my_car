077ea9ca0407b27fa78a81c8615e802c0cd81271
FukuInTheCode <tomc.code@gmail.com>
2023-09-18 22:09:44 +0200

    bug  fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@ int main(int argc, char* argv[])
     my_map_t map;
     my_car_t carz;
     my_car_create((void *)&carz, (void *)&map);
-    printf("%lf %lf\n", carz.pos.x, carz.pos.y);
     my_map_init(&map);
     sfVideoMode mode = {2500, 1500, 32};
     my_map_create(&map, mode);
