405210168afd01a8893e2ef0f473cb24592cd5eb
FukuInTheCode <tomc.code@gmail.com>
2023-09-18 22:53:24 +0200

    change in draw car

--- src/func/my_car_show.c
+++ src/func/my_car_show.c
@@ -6,6 +6,7 @@ void my_car_draw(sfRenderWindow *window, my_car_t *car, my_map_t *car)
     sfRectangleShape_setSize(car_rect, car->size);
     sfRectangleShape_setFillColor(car_rect, sfRed);
     sfVector2f car_origin = {car->size.x / 2., car->size.y / 2.};
-    sfRectangleShape_setOrigin(car_rect, car_origin)
+    sfRectangleShape_setOrigin(car_rect, car_origin);
+    sfRenderWindow_drawRectangleShape(window, car_rect, NULL);
     sfRectangleShape_destroy(car_rect);
 }
