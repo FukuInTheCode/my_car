afa95413574309c583787dfccc23d55a8421be4b
FukuInTheCode <tomc.code@gmail.com>
2023-09-18 23:13:36 +0200

    bug fix

--- src/func/my_car_show.c
+++ src/func/my_car_show.c
@@ -3,6 +3,7 @@
 void my_car_draw(sfRenderWindow *window, my_car_t *car, my_map_t *map)
 {
     sfRectangleShape *car_rect = sfRectangleShape_create();
+    sfRectangleShape_setPosition(car_rect, car->pos);
     sfRectangleShape_setSize(car_rect, car->size);
     sfRectangleShape_setFillColor(car_rect, sfRed);
     sfVector2f car_origin = {car->size.x / 2., car->size.y / 2.};
