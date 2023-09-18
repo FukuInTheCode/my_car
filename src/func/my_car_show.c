86329c7d71c6357c83d4716a6de9febeb9b42633
FukuInTheCode <tomc.code@gmail.com>
2023-09-18 22:51:49 +0200

    chanegs in draw

--- src/func/my_car_show.c
+++ src/func/my_car_show.c
@@ -2,5 +2,10 @@
 
 void my_car_draw(sfRenderWindow *window, my_car_t *car, my_map_t *car)
 {
-
+    sfRectangleShape *car_rect = sfRectangleShape_create();
+    sfRectangleShape_setSize(car_rect, car->size);
+    sfRectangleShape_setFillColor(car_rect, sfRed);
+    sfVector2f car_origin = {car->size.x / 2., car->size.y / 2.};
+    sfRectangleShape_setOrigin(car_rect, car_origin)
+    sfRectangleShape_destroy(car_rect);
 }
