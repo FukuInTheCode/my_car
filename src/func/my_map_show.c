24623fbfa7b9a7036b33f387a3025a6177696f47
FukuInTheCode <tomc.code@gmail.com>
2023-09-19 20:39:46 +0200

    add a start rect

--- src/func/my_map_show.c
+++ src/func/my_map_show.c
@@ -7,4 +7,10 @@ void my_map_draw(my_map_t *map, sfRenderWindow *window)
         sfRenderWindow_drawVertexArray(window, map->walls[i], NULL);
     for (uint32_t i = 0; i < map->gates_n; ++i)
         sfRenderWindow_drawVertexArray(window, map->gates[i], NULL);
+    sfRectangleShape *start_rect = sfRectangleShape_create();
+    sfVector2f rect_size = {30, 5};
+    sfRectangleShape_setSize(start_rect, rect_size);
+    sfRectangleShape_setPosition(start_rect, map->start);
+    sfRenderWindow_drawRectangleShape(window, start_rect, NULL);
+    sfRectangleShape_destroy(start_rect);
 }
