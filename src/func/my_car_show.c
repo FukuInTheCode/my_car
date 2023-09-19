177673db909e8b4764eaa4394982e746b8f63baf
FukuInTheCode <tomc.code@gmail.com>
2023-09-19 20:47:17 +0200

    changes in show

--- src/func/my_car_show.c
+++ src/func/my_car_show.c
@@ -4,10 +4,10 @@ void my_car_draw(sfRenderWindow *window, void *car_ptr, void *map_ptr)
 {
     my_car_t *car = (my_car_t *)car_ptr;
     sfRectangleShape *car_rect = sfRectangleShape_create();
-    sfRectangleShape_setPosition(car_rect, car->pos);
-    sfRectangleShape_setSize(car_rect, car->size);
-    sfRectangleShape_setFillColor(car_rect, sfRed);
+    sfRectangleShape_setPosition(car->rect, car->pos);
+    sfRectangleShape_setSize(car->rect, car->size);
+    sfRectangleShape_setFillColor(car->rect, sfRed);
     sfVector2f car_origin = {car->size.x / 2., car->size.y / 2.};
-    sfRectangleShape_setOrigin(car_rect, car_origin);
-    sfRenderWindow_drawRectangleShape(window, car_rect, NULL);
+    sfRectangleShape_setOrigin(car->rect, car_origin);
+    sfRenderWindow_drawRectangleShape(window, car->rect, NULL);
 }
