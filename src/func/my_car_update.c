626d18bf65ec037ce483e628b4656a46abbfd2f5
FukuInTheCode <tomc.code@gmail.com>
2023-09-19 21:00:55 +0200

    bug fix

--- src/func/my_car_update.c
+++ src/func/my_car_update.c
@@ -2,23 +2,23 @@
 
 uint32_t my_car_update(my_car_t *car, void *pop, uint32_t pop_size, my_map_t *map)
 {
-    if (car->acceleration < 0 && draw_car)
+    if (car->acceleration < 0)
         car->acceleration = my_min_between(car->acceleration + 0.3, 0);
-    else if (car->acceleration > 0 && draw_car)
+    else if (car->acceleration > 0)
         car->acceleration = my_max_between(car->acceleration - 0.3, 0);
     car->velocity += car->acceleration;
-    if (car->velocity < 0 && draw_car)
+    if (car->velocity < 0)
         car->velocity = my_min_between(car->velocity + 0.3, 0);
-    else if (car->velocity > 0 && draw_car)
+    else if (car->velocity > 0)
         car->velocity = my_max_between(car->velocity - 0.3, 0);
-    if (car->angle_acceleration < 0 && draw_car)
+    if (car->angle_acceleration < 0)
         car->angle_acceleration = my_min_between(car->angle_acceleration + 0.4, 0);
-    else if (car->angle_acceleration > 0 && draw_car)
+    else if (car->angle_acceleration > 0)
         car->angle_acceleration = my_max_between(car->angle_acceleration - 0.4, 0);
     car->angle_velocity += car->angle_acceleration;
-    if (car->angle_velocity < 0 && draw_car)
+    if (car->angle_velocity < 0)
         car->angle_velocity = my_min_between(car->angle_velocity + 0.4, 0);
-    else if (car->angle_velocity > 0 && draw_car)
+    else if (car->angle_velocity > 0)
         car->angle_velocity = my_max_between(car->angle_velocity - 0.4, 0);
     sfRectangleShape_rotate(car->rect, car->angle_velocity);
     double angle = sfRectangleShape_getRotation(car->rect) * PI / 180.;
