7d5fdd9e0db560cb53841a2a841703e47358549c
FukuInTheCode <tomc.code@gmail.com>
2023-09-19 20:47:46 +0200

    chnaeg in draw car

--- src/func/my_car_show.c
+++ src/func/my_car_show.c
@@ -3,11 +3,5 @@
 void my_car_draw(sfRenderWindow *window, void *car_ptr, void *map_ptr)
 {
     my_car_t *car = (my_car_t *)car_ptr;
-    sfRectangleShape *car_rect = sfRectangleShape_create();
-    sfRectangleShape_setPosition(car->rect, car->pos);
-    sfRectangleShape_setSize(car->rect, car->size);
-    sfRectangleShape_setFillColor(car->rect, sfRed);
-    sfVector2f car_origin = {car->size.x / 2., car->size.y / 2.};
-    sfRectangleShape_setOrigin(car->rect, car_origin);
     sfRenderWindow_drawRectangleShape(window, car->rect, NULL);
 }
