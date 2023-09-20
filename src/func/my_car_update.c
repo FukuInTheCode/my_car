1f5f2e56a311aa8e8c87850e090eae2fbef3b71f
FukuInTheCode <tomc.code@gmail.com>
2023-09-20 22:50:34 +0200

    change

--- src/func/my_car_update.c
+++ src/func/my_car_update.c
@@ -71,6 +71,7 @@ uint32_t my_car_update(void *car_ptr, void *pop, uint32_t pop_size, void *map_pt
         sfVector2f inter_pt;
         if (!my_map_is_intersecting_wall(map, line, &inter_pt))
             continue;
+        car->is_dead = true;
         break;
     }
     return pop_size;
