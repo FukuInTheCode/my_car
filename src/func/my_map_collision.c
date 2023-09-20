d6264549877d499ec911d5e51d578875a5d9bff7
FukuInTheCode <tomc.code@gmail.com>
2023-09-20 22:47:53 +0200

    bug fix

--- src/func/my_map_collision.c
+++ src/func/my_map_collision.c
@@ -5,7 +5,7 @@ bool my_map_is_intersecting_wall(my_map_t *map, sfVertex *line,\
                                                     sfVector2f *res)
 {
     bool is_intersect = false;
-    for (uint32_t i = 0; i < map->gates_n && !is_intersect; ++i) {
+    for (uint32_t i = 0; i < map->walls_n && !is_intersect; ++i) {
         size_t count = sfVertexArray_getVertexCount(map->walls[i]);
         for (uint32_t j = 0; j < count && !is_intersect; j += 2){
             sfVertex wall[] = {
