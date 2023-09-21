ba1a7138d134de5b76805f51d750b0acec107712
FukuInTheCode <tomc.code@gmail.com>
2023-09-21 23:13:04 +0200

    chanegs

--- src/func/my_car_show.c
+++ src/func/my_car_show.c
@@ -4,4 +4,8 @@ void my_car_draw(sfRenderWindow *window, void *car_ptr, void *map_ptr)
 {
     my_car_t *car = (my_car_t *)car_ptr;
     sfRenderWindow_drawRectangleShape(window, car->rect, NULL);
+    my_map_t *map = (my_map_t *)map_ptr;
+    if (map->tmp == NULL)
+        return;
+    map->tmp = window;
 }
