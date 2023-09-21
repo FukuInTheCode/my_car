d62578add7ba2a9d71224831aab66d45ec99cd20
FukuInTheCode <tomc.code@gmail.com>
2023-09-21 23:06:46 +0200

    chanegs

--- src/func/my_car_update.c
+++ src/func/my_car_update.c
@@ -85,7 +85,6 @@ uint32_t my_car_update(void *car_ptr, void *pop, uint32_t pop_size, void *map_pt
         };
         if (!my_map_is_intersecting_wall(map, line, &inter_vec))
             continue;
-        printf("See wall at: %lf deg\n", car->sight_angles[i]);
     }
     return pop_size;
 }
