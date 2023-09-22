dea1da978582f95d2876c4d49edd24bbb440394c
FukuInTheCode <tomc.code@gmail.com>
2023-09-22 22:40:24 +0200

    chnagein  main

--- src/main.c
+++ src/main.c
@@ -20,8 +20,8 @@ int main(int argc, char* argv[])
         }
         my_map_draw(window, (void *)&map);
         my_car_draw(window, (void *)&carz, (void *)&map);
-        sfRenderWindow_display(window);
         my_car_update(&carz, NULL, 0, &map);
+        sfRenderWindow_display(window);
     }
     sfRenderWindow_destroy(window);
 
