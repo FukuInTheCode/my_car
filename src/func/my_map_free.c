3368a102d3f22c4c40ed04e8075a44a80b82033a
FukuInTheCode <tomc.code@gmail.com>
2023-09-25 22:05:48 +0200

    change in free

--- src/func/my_map_free.c
+++ src/func/my_map_free.c
@@ -7,8 +7,11 @@ void my_map_free(my_map_t *map)
 
     free(map->walls);
     map->walls = NULL;
+    map->walls_n = 0;
     for (uint32_t i = 0; i < map->gates_n; i++)
         sfVertexArray_destroy(map->gates_n[i]);
     free(map->gates_n);
     map->gates = NULL;
+
+    map->gates_n = 0;
 }
